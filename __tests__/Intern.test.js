eab19324eca933647238eec997fab5c06cc3d52b
unknown <pratttrevor79@gmail.com>
2022-07-10 20:36:38 -0500

    creating new tests, worked them all out to pass

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,8 +1,27 @@
 const Intern = require('../lib/Intern');
-const intern = new Intern('joelle','5768098','jdeschat@gmail.com','Toronto');
+const intern = new Intern('Joelle','5768098','jdeschat@gmail.com','Toronto');
 
 test('Testing to see if the project can create a new instance of school', () => {
     const testSchool = 'Toronto';
     const employeeInstance = new Intern('Joelle', '5768098', 'jdeschat@gmail.com','Toronto');
     expect(employeeInstance.school).toBe(testSchool);
+});
+
+test('Tests if we can get the name from the getName() method', () => {
+    expect(intern.getName()).toBe('Joelle');
+});
+
+test('Tests if we can get the id number from the getId() method', () => {
+    expect(intern.getId()).toBe('5768098');
+});
+
+test('Tests if we can get the email from the getEmail() method', () => {
+    expect(intern.getEmail()).toBe('jdeschat@gmail.com');
+});
+
+test('Tests if we can get the role from getRole() method', () => {
+    expect(intern.getRole()).toBe('Intern');
+});
+test('Tests if we can get the school from getSchool() method', () => {
+    expect(intern.getSchool()).toBe('Toronto');
 });
