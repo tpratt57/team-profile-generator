68c168f285ba8504c8d16d1ca474b4f3d24861cf
unknown <pratttrevor79@gmail.com>
2022-07-10 20:18:53 -0500

    adding final passings tests to Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,5 @@
 const Employee = require('../lib/Employee');
-const emplpoyee = new Employee('Joelle', '5768098', 'jdeschat@gmail.com');
+const employee = new Employee('Joelle', '5768098', 'jdeschat@gmail.com');
 
 test('Is program capible of creating a new employee?', () => {
     const employeeInstance = new Employee;
@@ -7,7 +7,23 @@ test('Is program capible of creating a new employee?', () => {
 });
 
 test('Tests if we can get the constructor values for the employee object', () => {
-    expect(emplpoyee.name).toBe('Joelle');
+    expect(employee.name).toBe('Joelle');
     expect(employee.id).toBe('5768098');
     expect(employee.email).toBe('jdeschat@gmail.com');
+});
+
+test('Tests if we can get the name from the getName() method', () => {
+    expect(employee.getName()).toBe('Joelle');
+});
+
+test('Tests if we can get the id number from the getId() method', () => {
+    expect(employee.getId()).toBe('5768098');
+});
+
+test('Tests if we can get the email from the getEmail() method', () => {
+    expect(employee.getEmail()).toBe('jdeschat@gmail.com');
+});
+
+test('Tests if we can get the role from getRole method', () => {
+    expect(employee.getRole()).toBe('Employee');
 });
